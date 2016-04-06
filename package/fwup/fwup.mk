9cf1283c27bf142c4aa8759d2646bc9c9c1a395f
Frank Hunleth <fhunleth@troodon-software.com>
2016-04-06 12:06:33 -0400

    fwup: bump to version 0.6.1

--- package/fwup/fwup.mk
+++ package/fwup/fwup.mk
@@ -4,12 +4,14 @@
 #
 #############################################################
 
-FWUP_VERSION = v0.3.0
-FWUP_SITE = $(call github,fhunleth,fwup,$(FWUP_VERSION))
+FWUP_VERSION = 0.6.1
+FWUP_SITE = https://github.com/fhunleth/fwup/releases/download/v$(FWUP_VERSION)
 FWUP_LICENSE = Apache-2.0
-FWUP_LICENSE_FILES = COPYING
-FWUP_AUTORECONF = YES
+FWUP_LICENSE_FILES = LICENSE
 FWUP_DEPENDENCIES = libconfuse libarchive libsodium
+
+# help2man doesn't work when cross compiling
+FWUP_CONF_ENV = ac_cv_path_HELP2MAN=''
 
 $(eval $(autotools-package))
 $(eval $(host-autotools-package))
